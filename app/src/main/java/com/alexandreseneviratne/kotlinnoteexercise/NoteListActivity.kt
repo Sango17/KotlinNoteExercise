7787b7fce031d97e1daa99dabce9ada28152701f
Alex <alexandre.seneviratne@gmail.com>
2019-05-03 17:32:09 +0200

    set up recyclerview with adapter to the list of notes

--- app/src/main/java/com/alexandreseneviratne/kotlinnoteexercise/NoteListActivity.kt
+++ app/src/main/java/com/alexandreseneviratne/kotlinnoteexercise/NoteListActivity.kt
@@ -1,12 +1,49 @@
 package com.alexandreseneviratne.kotlinnoteexercise
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.alexandreseneviratne.kotlinnoteexercise.adapter.NoteListAdapter
+import kotlinx.android.synthetic.main.activity_note_list.*
 
-class NoteListActivity : AppCompatActivity() {
+class NoteListActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var notes: MutableList<Note>
+    private lateinit var adapter: NoteListAdapter
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
+
+        setSupportActionBar(toolbar)
+
+        notes = mutableListOf()
+
+        notes.add(Note("Hello", "World"))
+        notes.add(Note("Why Java ?", "Because Kotlin"))
+        notes.add(Note("Amanda", "Alex"))
+        notes.add(Note("Papa", "Maman"))
+
+        recyclerView = findViewById(R.id.note_list_recyclerview)
+        adapter = NoteListAdapter(notes, this)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        adapter.notifyDataSetChanged()
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.tag != null) {
+            val index: Int = view.tag as Int
+            val note: Note = notes[view.tag as Int]
+
+            Toast.makeText(this, note.title, Toast.LENGTH_SHORT).show()
+        }
     }
 }
