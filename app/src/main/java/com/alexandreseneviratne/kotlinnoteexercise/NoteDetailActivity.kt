b619d5729fc33ffd1bc9d496d0b1dacbdc7925c3
Alex <alexandre.seneviratne@gmail.com>
2019-05-03 17:46:55 +0200

    startActivityForResult to NoteDetailActivity when note_list_cardview clicked companion object added

--- app/src/main/java/com/alexandreseneviratne/kotlinnoteexercise/NoteDetailActivity.kt
+++ app/src/main/java/com/alexandreseneviratne/kotlinnoteexercise/NoteDetailActivity.kt
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class NoteDetailActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_NOTE: String = "note"
+        const val EXTRA_NOTE_INDEX: String = "noteIndex"
+
+        const val EDIT_NOTE_REQUEST_CODE: Int = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
