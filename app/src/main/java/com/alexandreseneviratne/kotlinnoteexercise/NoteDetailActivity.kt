5a613cad56576bdb36bb1c1d6c6b8d4f30f49e57
Alex <alexandre.seneviratne@gmail.com>
2019-05-03 18:28:14 +0200

    details displayed on activity_note_detail

--- app/src/main/java/com/alexandreseneviratne/kotlinnoteexercise/NoteDetailActivity.kt
+++ app/src/main/java/com/alexandreseneviratne/kotlinnoteexercise/NoteDetailActivity.kt
@@ -2,6 +2,8 @@ package com.alexandreseneviratne.kotlinnoteexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_note_detail.*
 
 class NoteDetailActivity : AppCompatActivity() {
     companion object {
@@ -11,8 +13,28 @@ class NoteDetailActivity : AppCompatActivity() {
         const val EDIT_NOTE_REQUEST_CODE: Int = 1
     }
 
+    private lateinit var noteTitle: TextView
+    private lateinit var noteText: TextView
+
+    private lateinit var note: Note
+    private var noteIndex: Int = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_detail)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        noteTitle = findViewById(R.id.note_detail_title)
+        noteText = findViewById(R.id.note_detail_text)
+
+        if (intent != null) {
+            note = intent.getParcelableExtra(EXTRA_NOTE)
+            noteIndex = intent.getIntExtra(EXTRA_NOTE_INDEX, -1)
+
+            noteTitle.text = note.title
+            noteText.text = note.text
+        }
     }
 }
